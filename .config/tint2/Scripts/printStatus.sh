5a67e5ef5745773fe2122f46364605dfec115264
ancooldrive <ancooldrive@gmail.com>
2020-06-01 21:08:50 +0200

    C

---
+++ .config/tint2/Scripts/printStatus.sh
@@ -0,0 +1,78 @@
+#! /bin/bash
+
+	#-------------------------------------------------------------------
+	function getDate
+	{
+		date +'%A %d %B %Y, %H:%M'
+	}
+
+	#-------------------------------------------------------------------
+	function getMemoryUsage
+	{
+		function print
+		{
+			function printInternal
+			{
+				out=$(awk "BEGIN {printf ($1)/$2}")
+				out=${out//[.]/,}
+				printf "  %."$3"\n" $out
+			}
+
+
+			if [ $(($1/1048576)) -gt 0 ]
+			then
+				printInternal $1 1048576 2fG
+			else
+				printInternal $1 1024 0fM
+			fi
+		}
+
+		free=($(free))
+		print $((${free[8]}+${free[10]}))
+	}
+
+
+	#-------------------------------------------------------------------
+	#todo
+	function getCpuUsage
+	{
+		echo  $(echo $[100-$(vmstat 1 2|tail -1|awk '{print $15}')])%
+		#echo  $(echo $[100-$(vmstat|tail -1|awk '{print $15}')])%
+	}
+
+
+	#-------------------------------------------------------------------
+	function getNetworkMonitor
+	{
+		logfile=${HOME}/.cache/netlog
+		prevdata="$(cat "$logfile")"
+
+		rxcurrent="$(($(cat /sys/class/net/*/statistics/rx_bytes | paste -sd '+')))"
+		txcurrent="$(($(cat /sys/class/net/*/statistics/tx_bytes | paste -sd '+')))"
+
+		printf "\uea92%sKiB/s \uea95%sKiB/s\\n" "$(((rxcurrent-${prevdata%% *})/(1024*5)))" "$(((txcurrent-${prevdata##* })/(1024*5)))"
+
+		echo "$rxcurrent $txcurrent" > "$logfile"
+	}
+
+	#-------------------------------------------------------------------
+	function getVolume
+	{
+		array=$(echo $(amixer get 'Master') | awk '{print $27 " " $26}')
+		array=${array//['[',']']/}
+		IFS=' ' read -r -a array <<< "$array"
+		if [ ${array[0]} = "on" ];
+		then
+			echo -e '\ueea8' ${array[1]}
+		else
+			echo -e '\uecb8' mute
+		fi
+	}
+
+#~ ---------------------------------------------------------------------
+#~ to color text use span
+#~ echo '<span foreground="red" background="black">text</span>'
+#~ ---------------------------------------------------------------------
+
+separator='<span> </span><span> </span><span> </span><span> </span>'
+echo -e $(getVolume)${separator}$(getNetworkMonitor)${separator}$(getCpuUsage)${separator}$(getMemoryUsage)${separator}$(getDate)
---
+++ push.sh
@@ -0,0 +1,55 @@
+# ------------------------------------------------------------------------------
+# COPING FILES
+# ------------------------------------------------------------------------------
+
+repPath=${HOME}"/.git/dotfiles"
+
+# .config
+
+configPath=${HOME}"/.config"
+configRepPath=${repPath}"/.config"
+
+mkdir -p ${configRepPath}
+
+rsync -var --delete ${configPath}/tint2 ${configRepPath}
+rsync -var --delete ${configPath}/openbox ${configRepPath}
+rsync -var --delete ${configPath}/pcmanfm ${configRepPath}
+rsync -var --delete ${configPath}/geany ${configRepPath}
+rsync -var --delete ${configPath}/alacritty ${configRepPath}
+rsync -var --delete ${configPath}/Atom ${configRepPath}
+rsync -var --delete ${configPath}/compton ${configRepPath}
+rsync -var --delete ${configPath}/gtk-2.0 ${configRepPath}
+rsync -var --delete ${configPath}/gtk-3.0 ${configRepPath}
+rsync -var --delete ${configPath}/i3 ${configRepPath}
+rsync -var --delete ${configPath}/nitrogen ${configRepPath}
+rsync -var --delete ${configPath}/rofi ${configRepPath}
+
+# .local
+
+localPath=${HOME}"/.local/share"
+localRepPath=${repPath}"/.local/share"
+
+mkdir -p ${localRepPath}
+
+rsync -var --delete ${localPath}/rofi ${localRepPath}
+
+# .themes
+
+themesPath=${HOME}"/.themes"
+
+rsync -var --delete ${themesPath} ${repPath}
+
+# home files
+
+cp ${HOME}/.bash_profile ${repPath}
+cp ${HOME}/.bashrc ${repPath}
+cp ${HOME}/.gtkrc-2.0 ${repPath}
+cp ${HOME}/.xinitrc ${repPath}
+
+# ------------------------------------------------------------------------------
+# PUSHING TO GIT
+# ------------------------------------------------------------------------------
+
+git add -A
+git commit -m "C"
+git push
