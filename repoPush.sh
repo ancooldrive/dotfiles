116fa8cd1d5edfdb8eb9e6b90e9461f1f0dd0560
ancooldrive <ancooldrive@gmail.com>
2020-06-02 08:18:20 +0200

    c

--- repoPush.sh
+++ repoPush.sh
@@ -4,57 +4,77 @@
 
 source ${HOME}"/.git/dotfiles/.repoScripts/config.sh"
 
+function copyFiles
+{
+  # .config
+
+  rm -rf ${configRepPath}
+  mkdir -p ${configRepPath}
+
+  rsync -var --delete ${configPath}/tint2 ${configRepPath}
+  rsync -var --delete ${configPath}/openbox ${configRepPath}
+  rsync -var --delete ${configPath}/pcmanfm ${configRepPath}
+  rsync -var --delete ${configPath}/geany ${configRepPath}
+  rsync -var --delete ${configPath}/alacritty ${configRepPath}
+  rsync -var --delete ${configPath}/compton ${configRepPath}
+  rsync -var --delete ${configPath}/gtk-2.0 ${configRepPath}
+  rsync -var --delete ${configPath}/gtk-3.0 ${configRepPath}
+  rsync -var --delete ${configPath}/i3 ${configRepPath}
+  rsync -var --delete ${configPath}/nitrogen ${configRepPath}
+  rsync -var --delete ${configPath}/rofi ${configRepPath}
+
+  # .local
+
+  rm -rf ${localRepPath}
+  mkdir -p ${localRepPath}
+
+  rsync -var --delete ${localPath}/rofi ${localRepPath}
+
+  # .themes
+
+  rsync -var --delete ${HOME}"/.themes" ${repPath}
+
+  # .wallpapers
+
+  rsync -var --delete ${HOME}"/.wallpapers" ${repPath}
+
+  # home files
+
+  cp ${HOME}/.bash_profile ${repPath}
+  cp ${HOME}/.bashrc ${repPath}
+  cp ${HOME}/.gtkrc-2.0 ${repPath}
+  cp ${HOME}/.xinitrc ${repPath}
+}
+
+function deleteLocalFiles
+{
+  rm -rf ${configRepPath}
+  rm -rf ${repPath}"/.local"
+  rm -rf ${repPath}"/.themes"
+  rm -rf ${repPath}"/.wallpapers"
+
+  rm ${repPath}/.bash_profile
+  rm ${repPath}/.bashrc
+  rm ${repPath}/.gtkrc-2.0
+  rm ${repPath}/.xinitrc
+}
+
+function gitPush
+{
+  git -C ${repPath} add -A
+  git -C ${repPath} status
+
+  read -p "Commit? [Y/n]: " doCommit
+  if [ "$doCommit" == "Y" ] || [ "$doCommit" == "" ]; then
+    git -C ${repPath} commit -m "c"
+    git -C ${repPath} push
+  fi
+}
+
 # ------------------------------------------------------------------------------
-# COPING FILES
+# MAIN SCRIPT
 # ------------------------------------------------------------------------------
 
-# .config
-
-rm -rf ${configRepPath}
-mkdir -p ${configRepPath}
-
-rsync -var --delete ${configPath}/tint2 ${configRepPath}
-rsync -var --delete ${configPath}/openbox ${configRepPath}
-rsync -var --delete ${configPath}/pcmanfm ${configRepPath}
-rsync -var --delete ${configPath}/geany ${configRepPath}
-rsync -var --delete ${configPath}/alacritty ${configRepPath}
-rsync -var --delete ${configPath}/compton ${configRepPath}
-rsync -var --delete ${configPath}/gtk-2.0 ${configRepPath}
-rsync -var --delete ${configPath}/gtk-3.0 ${configRepPath}
-rsync -var --delete ${configPath}/i3 ${configRepPath}
-rsync -var --delete ${configPath}/nitrogen ${configRepPath}
-rsync -var --delete ${configPath}/rofi ${configRepPath}
-
-# .local
-
-rm -rf ${localRepPath}
-mkdir -p ${localRepPath}
-
-rsync -var --delete ${localPath}/rofi ${localRepPath}
-
-# .themes
-
-rsync -var --delete ${HOME}"/.themes" ${repPath}
-
-# .wallpapers
-
-rsync -var --delete ${HOME}"/.wallpapers" ${repPath}
-
-# home files
-
-cp ${HOME}/.bash_profile ${repPath}
-cp ${HOME}/.bashrc ${repPath}
-cp ${HOME}/.gtkrc-2.0 ${repPath}
-cp ${HOME}/.xinitrc ${repPath}
-
-# ------------------------------------------------------------------------------
-# PUSHING TO GIT
-# ------------------------------------------------------------------------------
-git add -A
-git status
-
-read -p "Commit? [Y/n]: " doCommit
-if [ "$doCommit" == "Y" ] || [ "$doCommit" == "" ]; then
-  git commit -m "c"
-  git push
-fi
+copyFiles
+gitPush
+deleteLocalFiles
